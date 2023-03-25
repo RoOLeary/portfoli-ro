4d6ee27db244d4acfaee329371c182be33565b7e
Ronan <roleary81@gmail.com>
2023-03-25 23:30:07 +0100

    edit cols

--- src/partials/Columns.jsx
+++ src/partials/Columns.jsx
@@ -7,7 +7,7 @@ function Columns() {
   return (
     <>
 
-      <div className="flex flex-wrap md:flex-nowrap md:flex max-w-6xl mx-auto px-4 sm:px-6 gap-2 mb-12">
+      <div className="flex flex-wrap md:flex-nowrap md:flex max-w-6xl mx-auto px-4 sm:px-6 gap-4 mb-12">
         <div className='column one'><h3 className="text-white font-bold">Column 1</h3></div>
         <div className='column two'><h3 className="text-white font-bold">Column 2</h3></div>
       </div>
