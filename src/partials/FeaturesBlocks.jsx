3df66c7b6944eab0f9050d1b05e99b3faf90fdd4
Ronan <roleary81@gmail.com>
2023-03-25 11:58:27 +0100

    make some subtle improvements to the main site

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -13,7 +13,7 @@ function FeaturesBlocks() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">My process. Six steps to building stuff.</h2>
+            <h2 className="h2 mb-4">My process. Six steps to building cool stuff.</h2>
           </div>
 
           {/* Items */}
