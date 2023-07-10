ae81c3f2b315d836258105e9e4a116df13bff225
Ronan <roleary81@gmail.com>
2023-07-10 21:25:57 +0200

    fix typo

--- src/partials/Features.jsx
+++ src/partials/Features.jsx
@@ -110,7 +110,7 @@ function Features() {
                 >
                   <div>
                     <div className="font-bold leading-snug tracking-tight mb-1">Busy Little Pixels</div>
-                    <div className="text-gray-600">CMS and application development for Fractional CTO concernn based in Dublin. Craft CMS, NextJS 13.4+</div>
+                    <div className="text-gray-600">CMS and application development for Fractional CTO concern based in Dublin. Craft CMS, NextJS 13.4+</div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
                     <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
