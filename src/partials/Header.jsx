97c8a7e1790028d8b00e623e56580e1300bddcd5
Ronan <roleary81@gmail.com>
2024-07-12 13:04:07 +0200

    some edits

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
 
   return (
     <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
-      <Banner text="🇪🇸 ¡VAMOS ESPAÑA! 🇪🇸" />
+      <Banner text="🇪🇸 🏆 ¡VAMOS ESPAÑA! 🏆 🇪🇸" />
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
