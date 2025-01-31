11e7cdd401aaded39f6b6568ba9be8212bd36183
Ronan O'Leary <roleary81@gmail.com>
2025-01-31 13:09:10 +0100

    did this fix it

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -8,8 +8,7 @@ function HeroHome() {
   const [videoModalOpen, setVideoModalOpen] = useState(false);
 
   return (
-    <section className="relative">
-
+    <section className="relative border-none">
       {/* Illustration behind hero content */}
       <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
         <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
