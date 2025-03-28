bf736e8b1f2af28c1cf9d3535f12a27b0282199c
Ronan O'Leary <roleary81@gmail.com>
2025-03-28 10:15:36 +0100

    you must be kidding

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -35,7 +35,7 @@ function HeroHome() {
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Hi, I'm Ronan,  <br />& I make <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-300 to-red-600">websites.</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Originally from 🇮🇪, based in 🇳🇱,  I'm a versatile full-stack developer, manager and Technical Lead over 15 years experience developing, scaling and optimising large scale websites and apps. My current favourite tools/weapons of choice: Next JS/React (Typescript), React-Native, Craft CMS, Python, Shopify, Laravel, Supabase, Node JS and Tailwind...to name but a few.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Originally from 🇮🇪, based in 🇳🇱, I'm a versatile full-stack developer, manager, CTO and Technical Lead with over 15 years experience developing, scaling and optimising large scale websites and apps. My current favourite tools/weapons of choice: Next JS/React (Typescript), React-Native, Craft CMS, Python, Shopify, Laravel, Supabase, Node JS and Tailwind...to name but a few.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-red-600 hover:bg-red-700 w-full mb-4 sm:w-auto sm:mb-0" href="#about">Skills/Experience</a>
