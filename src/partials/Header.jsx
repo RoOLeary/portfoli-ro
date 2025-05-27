60c04ddf6133c634ead35573c246def8c1949b57
Ronan O'Leary <roleary81@gmail.com>
2025-05-27 12:48:50 +0200

    kill banner for now

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
 
   return (
     <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
-      <Banner text="LIVERPOOL FC - PREMIER LEAGUE CHAMPIONS 2024/25 - YNWA" />
+      {/* <Banner text="LIVERPOOL FC - PREMIER LEAGUE CHAMPIONS 2024/25 - YNWA" /> */}
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
